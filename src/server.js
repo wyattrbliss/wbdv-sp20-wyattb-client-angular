09f65f050d21d1057073ba20fb283af97da8d4b2
wyatt <wyattrbliss@gmail.com>
2020-04-21 19:55:38 -0400

    assignment 7

--- src/server.js
+++ src/server.js
@@ -4,12 +4,12 @@ const path = require('path');
 
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/wbdv-sp20-wyattb-a7-angular/a7wb'));
+// Serve only the static files from the dist directory
+app.use(express.static(__dirname + '/dist/wbdv-sp20-wyattb-a7-angular'));
 
 app.get('/*', function(req,res) {
 
-res.sendFile(path.join(__dirname+'/dist/wbdv-sp20-wyattb-a7-angular/index.html'));
+res.sendFile(path.join(__dirname + 'dist/wbdv-sp20-wyattb-a7-angular/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
